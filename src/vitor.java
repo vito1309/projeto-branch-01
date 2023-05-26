ccf3177959ad6e7d86e2e8f9c5b28db030769c82
vito <vitornegri2005@gmail.com>
2023-05-25 21:45:04 -0300

    vatefodei

---
+++ src/vitor.java
@@ -0,0 +1,5 @@
+public class vitor {
+    public static void main(String[] args) {
+    System.out.println("bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota bota na bota na bota na pipokinha");        
+    }
+}
